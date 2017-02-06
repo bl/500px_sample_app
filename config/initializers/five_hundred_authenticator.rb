d19cdec49feedf17d8d04fad07e480d310b9ca5b
Bernard Laveaux <blaveaux@outlook.com>
2017-02-06 18:05:29 -0500

    Fallback 500px consumer key/secret from env variables

--- config/initializers/five_hundred_authenticator.rb
+++ config/initializers/five_hundred_authenticator.rb
@@ -1,1 +1,2 @@
-FiveHundred::Authenticator.configure(Settings.fivehundred || {})
+settings = Settings.fivehundred || { consumer_key: ENV['FIVE_HUNDRED_CONSUMER_KEY'], consumer_secret: ENV['FIVE_HUNDRED_CONSUMER_SECRET'] }
+FiveHundred::Authenticator.configure(settings)
